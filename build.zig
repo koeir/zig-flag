3c464dfe3797b0e0256b50ce00da5e1139b46c88
koeir <kianjohnban@gmail.com>
2026-04-14 22:47:42 +0800

    chore: added build and run for executable example

--- build.zig
+++ build.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const flagparse = b.addModule("flagparse", .{
         .root_source_file = b.path("src/root.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .target = target,
+        .optimize = optimize,
     });
 
     const lib = b.addLibrary(.{
@@ -13,4 +16,24 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(lib);
+
+    const exe = b.addExecutable(.{
+        .name = "example",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        })
+    });
+
+    exe.root_module.addImport("flagparse", flagparse);
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "run");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 }
