bbf2e5776c43c6a5789a51f6e41c284954ee7f1d
koeir <kianjohnban@gmail.com>
2026-04-17 08:43:50 +0800

    log: integrated allocation

--- src/root.zig
+++ src/root.zig
@@ -4,13 +4,14 @@ pub const Type = @import("Type.zig");
 
 // arg.index is not reset when unsuccessful
 pub fn parse(
+    allocator: *std.mem.Allocator,
     args: *const std.process.Args,
-    argbuf: [][:0]const u8,
     comptime init_flags: Type.Flags,
-    out_flags: []Type.Flag,
     errorbuf: ?[]u8,
     cfg: Type.ParseConfig,
-    ) !struct { flags: Type.Flags, argv: [][:0]const u8 } {
+) !struct { flags: Type.Flags, argv: *[]*[:0]const u8 } {
+    if (cfg.verbose == true and cfg.writer == null) return error.NoWriter;
+    defer cfg.writer.?.flush() catch {};
 
     var iter = args.iterate();
     var args_iter: Type.ArgIterator = .{
@@ -19,35 +20,27 @@ pub fn parse(
         .count = args.vector.len,
     };
 
-    if (args_iter.count > argbuf.len) {
-        return error.OutOfMemory;
-    }
+    // Initialize the parsed flags
+    var out_flags = try allocator.alloc(Type.Flag, init_flags.list.len);
+    for (init_flags.list, 0..) |value, i| out_flags[i] = value;
 
-    // Should be compile error really but out_flags must be a runtime var
-    if (out_flags.len != init_flags.list.len) {
-        return error.OutOfMemory;
-    }
-
-    if (cfg.verbose == true and cfg.writer == null) {
-        return error.NoWriter;
-    } else {
-        defer cfg.writer.?.flush() catch {};
-    }
-
-    // Initialize the output flags for mutation
-    for (init_flags.list, 0..) |value, i| {
-        out_flags[i] = value;
-    }
+    // argbuf for args array without flags
+    var argbuf = try allocator.alloc(*[:0]u8, args.vector.len);
 
     // Init struct for simpler syntax
     const OutArgs = struct {
-        arg: [][:0]const u8,
+        arg: []*[:0]const u8,
         index: usize = 0,
+
+        pub fn add_arg(self: *@This(), arg: *[:0]const u8) void {
+            self.arg[self.index] = arg;
+            self.index += 1;
+        }
     };
 
     // Use buffer
     var out_args: OutArgs = .{
-        .arg = argbuf,
+        .arg = &argbuf,
     };
 
     if (!args_iter.skip()) return error.NoArgs;
@@ -58,40 +51,18 @@ pub fn parse(
             // note that if the current flag is an argumentative,
             // it takes the next arg, which wouldn't go into this
             // slice
-            out_args.arg[out_args.index] = arg.*;
-            out_args.index += 1;
+
+            out_args.add_arg(arg);
             continue;
         };
 
         switch (fmt) {
-            .Short  => helpers.parse_chain(&args_iter, out_flags, init_flags, cfg) catch |err| {
-                // If its argnoarg and the end of argv hasn't been reached yet,
-                // the next arg *must* have been a flag, so -1 so that arg.index
-                // is on the erred flag
-                if (err == Type.FlagErrs.ArgNoArg and
-                args_iter.index != args_iter.count) args_iter.index -= 1;
-
-                if (errorbuf) |e| {
-                    const current_arg = args_iter.current().?;
-
-                    if (e.len < current_arg.len) return error.OutOfMemory;
-                    @memcpy(e[0..current_arg.len], current_arg);
-                }
-
+            .Short => helpers.parse_chain(&args_iter, out_flags, init_flags, cfg) catch |err| {
+                try helpers.put_error(&errorbuf, err, &args_iter);
                 return err;
             },
-            .Long   => helpers.parse_long(&args_iter, out_flags, init_flags, cfg) catch |err| {
-                // See comment directly above
-                if (err == Type.FlagErrs.ArgNoArg and
-                args_iter.index != args_iter.count) args_iter.index -= 1;
-
-                if (errorbuf) |e| {
-                    const current_arg = args_iter.current().?;
-
-                    if (e.len < current_arg.len) return error.OutOfMemory;
-                    @memcpy(e[0..current_arg.len], current_arg);
-                }
-
+            .Long => helpers.parse_long(&args_iter, out_flags, init_flags, cfg) catch |err| {
+                try helpers.put_error(&errorbuf, err, &args_iter);
                 return err;
             },
         }
@@ -101,7 +72,7 @@ pub fn parse(
         .list = out_flags,
     };
 
-    return .{ .flags = ret, .argv = out_args.arg[0..out_args.index] };
+    return .{ .flags = ret, .argv = &out_args.arg };
 }
 
 // Returns whether if a flag is in long or short form
